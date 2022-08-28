fff6343d79b776135a2d9db33ccae097d9003b2f
ElenaLatynina <latynina.elena.v@gmail.com>
2022-08-28 11:17:30 +0300

    markup

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,6 @@ import '../css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries';
+import { countriesListMarkup, countryCardMarkup } from './markup';
 
 const DEBOUNCE_DELAY = 300;
